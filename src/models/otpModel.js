cffd47d134f48a044e962c5d4a415cf9fe4c9a84
Ganz <gandarainpanjaitan@Ganzs-Mac-mini.local>
2025-11-01 15:03:42 +0700

    enhance otp

--- src/models/otpModel.js
+++ src/models/otpModel.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose'
 
+import { OTP_TYPE } from '../constants/otpTypes.js'
+
 const otpSchema = new mongoose.Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
+  type: { type: String, enum: [OTP_TYPE.REGISTER, OTP_TYPE.FORGOT_PASSWORD], required: true },
+  expiresAt: { type: Date, required: true },
+  isVerified: { type: Boolean, default: false },
 })
 
 export default mongoose.model('Otp', otpSchema)
